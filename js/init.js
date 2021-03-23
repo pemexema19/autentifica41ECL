7bc0b2ef4b527a95e044525ab58fe5ef152c3629
pemexema19 <81199648+pemexema19@users.noreply.github.com>
2021-03-23 10:46:15 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBi5Yx-NVhKdKeqR1qE83VFr7dzZmtrzws",
+    authDomain: "autentifica41ecl.firebaseapp.com",
+    projectId: "autentifica41ecl",
+    storageBucket: "autentifica41ecl.appspot.com",
+    messagingSenderId: "758364802517",
+    appId: "1:758364802517:web:ae6ae6075d19fbec74415c",
+    measurementId: "G-NWN0GSVPLS"
+    });
